247e5e778ac3103292442786aa0f1e699e26b8a7
yoonajoo <70946756+yoonajoo@users.noreply.github.com>
2022-02-11 23:07:50 +0900

    pythondef.py

---
+++ pythondef.py
@@ -0,0 +1,17 @@
+# 2.3 함수
+def get_min_order(ticket) :
+    min_order = None 
+    if ticket == "ETC" :
+        min_order = 0.1
+    elif ticket == "ETH" :
+         min_order = 0.5
+    elif ticket == "BTC" :
+         min_order = 0.01
+    elif ticket == "XRP" :
+         min_order = 10
+    else :
+        min_order = 0.005
+    return min_order
+
+min_order = get_min_order("BTC")
+print(min_order)
