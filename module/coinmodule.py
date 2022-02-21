0f01ad85c5e2d8946e12ec087c7934e288d68bb6
yoonajoo <70946756+yoonajoo@users.noreply.github.com>
2022-02-21 21:50:51 +0900

    module

---
+++ module/coinmodule.py
@@ -0,0 +1,13 @@
+# 86. Let's make a module!
+ohlc = {
+    "BTC": [100, 200, 300, 400],
+    "ETH": [200, 300, 400, 500]
+}
+
+def get_open_price(currency):
+    return ohlc[currency][0]
+
+def get_close_price(currency):
+    return ohlc[currency][3]
+
+
---
+++ module/modulecall.py
@@ -0,0 +1,24 @@
+# 86. call module test
+import coinmodule
+
+callbtc = coinmodule.get_open_price("BTC")
+print(callbtc)
+
+calleth = coinmodule.get_close_price("ETH")
+print(calleth)
+
+# 89. datetime module
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+
+# 90. datetime.timedelta()
+print(now + datetime.timedelta(hours=10))
+print(now - datetime.timedelta(minutes=30))
+
+# 91. requests module (anaconda에서 실행)
+import requests
+resp = requests("https://api.bithumb.com/public/ticker/BTC")
+print(resp)
+print(resp.content)
